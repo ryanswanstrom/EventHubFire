2a6b18aebd4146c67e3263824b13ea9844aeb3e0
ryanswanstrom <ryanswanstrom@users.noreply.github.com>
2016-10-19 22:12:53 -0500

    adding some code and dependencies

--- EventHubFire/Program.cs
+++ EventHubFire/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace EventHubFire
 {
+    using Microsoft.ServiceBus.Messaging;
+
     class Program
     {
         static void Main(string[] args)
         {
+            //use the connection string from a newly created shared access policy on the event hub
+            var ehClient = EventHubClient.CreateFromConnectionString("<connection-string>");
+            ehClient.SendAsync(new EventData(Encoding.UTF8.GetBytes("Hello World, This is a message.")));
+
+            Console.WriteLine("Message sent");
+            Console.ReadLine();
         }
     }
 }
